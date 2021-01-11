9b6b80e5fef27700bf79aeb4108a42b6765af96a
Dominik Madarász <zaklaus@outlook.com>
2021-01-11 17:29:18 +0100

    Add shared atlas header

--- code/apps/server/source/blocks_list.c
+++ code/apps/server/source/blocks_list.c
@@ -1,7 +1,8 @@
 #include "blocks.h"
+#include "atlas_shared.h"
 
 static block blocks[] = {
-    {.tex_id = 0, .name = "base-ground", .flags = 0, .kind = BLOCK_KIND_GROUND, .biome = 0, .symbol = '.'},
-    {.tex_id = 1, .name = "base-wall", .flags = BLOCK_FLAG_COLLISION, .kind = BLOCK_KIND_WALL, .biome = 0, .symbol = '#'},
-    {.tex_id = 2, .name = "base-water", .flags = BLOCK_FLAG_COLLISION, .kind = BLOCK_KIND_WATER, .biome = 0, .symbol = '~'},
+    {.tex_id = ATLAS_XY(0, 0), .name = "base-ground", .flags = 0, .kind = BLOCK_KIND_GROUND, .biome = 0, .symbol = '.'},
+    {.tex_id = ATLAS_XY(1, 0), .name = "base-wall", .flags = BLOCK_FLAG_COLLISION, .kind = BLOCK_KIND_WALL, .biome = 0, .symbol = '#'},
+    {.tex_id = ATLAS_XY(0, 1), .name = "base-water", .flags = BLOCK_FLAG_COLLISION, .kind = BLOCK_KIND_WATER, .biome = 0, .symbol = '~'},
 };
---
+++ code/common/atlas_shared.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#define ATLAS_STRIDE 10
+#define ATLAS_XY(x,y) ((y*ATLAS_STRIDE)+x)
