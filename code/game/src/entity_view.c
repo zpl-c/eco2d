83fa4b6daf61c9bddbbb620511123b89f76fd10f
Dominik Madarász <zaklaus@madaraszd.net>
2021-08-09 15:59:50 +0200

    sync: skip hp data for chunks

--- code/game/src/entity_view.c
+++ code/game/src/entity_view.c
@@ -9,13 +9,18 @@ pkt_desc pkt_entity_view_desc[] = {
     { PKT_UINT(entity_view, flag) },
     { PKT_HALF(entity_view, x) },
     { PKT_HALF(entity_view, y) },
+    
     { PKT_SKIP_IF(entity_view, blocks_used, 1, 2) }, // NOTE(zaklaus): skip velocity for chunks
     { PKT_HALF(entity_view, vx) },
     { PKT_HALF(entity_view, vy) },
+    
     { PKT_SKIP_IF(entity_view, blocks_used, 0, 1) }, // NOTE(zaklaus): skip blocks for anything else
     { PKT_ARRAY(entity_view, blocks) },
+    
+    { PKT_SKIP_IF(entity_view, blocks_used, 1, 2) }, // NOTE(zaklaus): skip hp for chunks
     { PKT_HALF(entity_view, hp) },
     { PKT_HALF(entity_view, max_hp) },
+    
     { PKT_END }, 
 };
 
