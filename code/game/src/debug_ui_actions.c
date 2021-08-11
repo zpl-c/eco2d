0a6f2485a2662557f086e7ed646ec5fd2763c620
Dominik Madarász <zaklaus@madaraszd.net>
2021-08-11 21:51:15 +0200

    fix: small changes

--- code/game/src/debug_ui_actions.c
+++ code/game/src/debug_ui_actions.c
@@ -1,12 +1,12 @@
 #include "debug_ui.h"
 #include "world/blocks.h"
 
-static inline void
+void
 ActExitGame(void) {
     game_request_close();
 }
 
-static inline void
+void
 ActSpawnCar(void) {
     ecs_entity_t e = vehicle_spawn();
     ecs_entity_t plr = camera_get().ent_id;
@@ -18,7 +18,7 @@ ActSpawnCar(void) {
     debug_replay_special_action(RPKIND_SPAWN_CAR);
 }
 
-static inline void
+void
 ActPlaceIceRink(void) {
     ecs_entity_t plr = camera_get().ent_id;
     uint8_t watr_id = blocks_find(BLOCK_BIOME_DEV, BLOCK_KIND_WATER);
@@ -37,64 +37,64 @@ ActPlaceIceRink(void) {
 
 // NOTE(zaklaus): Replay system
 
-static inline uint8_t
+uint8_t
 CondReplayStatusOn(void) {
     return is_recording && !is_playing;
 }
 
-static inline uint8_t
+uint8_t
 CondReplayStatusOff(void) {
     return !is_recording && !is_playing;
 }
 
-static inline uint8_t
+uint8_t
 CondReplayDataPresentAndNotPlaying(void) {
     return records != NULL && !is_recording && !is_playing;
 }
 
-static inline uint8_t
+uint8_t
 CondReplayIsPlaying(void) {
     return records != NULL && !is_recording && is_playing;
 }
 
-static inline uint8_t
+uint8_t
 CondReplayIsNotPlaying(void) {
     return !is_recording && !is_playing;
 }
 
-static inline uint8_t
+uint8_t
 CondReplayIsNotPlayingOrRecordsNotClear(void) {
     return records != NULL && !is_recording && !is_playing;
 }
 
-static inline void
+void
 ActReplayBegin(void) {
     debug_replay_start();
 }
 
-static inline void
+void
 ActReplayEnd(void) {
     debug_replay_stop();
 }
 
-static inline void
+void
 ActReplayRun(void) {
     debug_replay_run();
 }
 
-static inline void
+void
 ActReplayClear(void) {
     debug_replay_clear();
 }
 
 
-static inline void
+void
 ActReplayNew(void) {
     debug_replay_clear();
     zpl_zero_size(replay_filename, sizeof(replay_filename));
 }
 
-static inline void
+void
 ActReplaySaveAs(void) {
     if (!records) return;
     char const *workdir = GetWorkingDirectory();
@@ -116,7 +116,7 @@ ActReplaySaveAs(void) {
     
 }
 
-static inline void
+void
 ActReplaySave(void) {
     if (!replay_filename[0]) {
         ActReplaySaveAs();
@@ -124,7 +124,7 @@ ActReplaySave(void) {
     else debug_replay_store();
 }
 
-static inline void
+void
 ActReplayLoad(void) {
     char const *workdir = GetWorkingDirectory();
     
@@ -149,7 +149,7 @@ ActReplayLoad(void) {
 // NOTE(zaklaus): Demo NPCs
 static ecs_entity_t *demo_npcs = NULL;
 
-static inline void
+void
 ActSpawnDemoNPCs(void) {
     if (!demo_npcs) zpl_array_init(demo_npcs, zpl_heap());
     if (zpl_array_count(demo_npcs) >= 10000) return;
@@ -169,7 +169,7 @@ ActSpawnDemoNPCs(void) {
     }
 }
 
-static inline void
+void
 ActDespawnDemoNPCs(void) {
     if (!demo_npcs) return;
     
