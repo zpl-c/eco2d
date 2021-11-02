2130d85edf9cf9ff2b92e594aa013e1222778326
Dominik Madarász <zaklaus@outlook.com>
2021-11-02 13:02:08 +0100

    info: asset_setup.h for quick nav

---
+++ code/game/src/asset_setup.h
@@ -0,0 +1,20 @@
+#error DO NOT INCLUDE THIS FILE
+
+// NOTE(zaklaus): This file acts as a shortcut for setting up assets in the eco2d
+// use your favorite editor to quickly navigate between various files.
+
+// 1) Register a new Asset ID
+#include "assets.h"
+
+// 2) Add the asset to the asset list
+#include "assets_list.c"
+
+// NOTE(zaklaus): Now that your asset is registered, we
+// can use it in other systems
+
+// NOTE(zaklaus): Register a block
+#include "blocks_list.c"
+
+// NOTE(zaklaus): Register an item
+#include "items_list.c"
+
