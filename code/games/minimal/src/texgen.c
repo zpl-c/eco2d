583535c138d379db965657197fad5c72a2511930
Dominik Madarász <zaklaus@outlook.com>
2022-09-29 14:17:47 +0200

    minimal texgen

--- code/games/minimal/src/texgen.c
+++ code/games/minimal/src/texgen.c
@@ -6,13 +6,7 @@
 Texture2D texgen_build_anim(asset_id id, int64_t counter) {
     (void)counter;
     switch (id) {
-        case ASSET_WATER: {
-            Image img = LoadImageEco("water");
-            ImageColorBrightness(&img, zpl_abs((counter % 64 - 32)*2));
-            return Image2TexEco(img);
-        }break;
-
-        default: return GenColorEco(PINK); break;
+        default: return GenColorEco(ColorFromHSV(id * 240.0f, 0.6f, 0.45f)); break;
     }
 }
 
@@ -21,30 +15,6 @@ Texture2D texgen_build_sprite(asset_id id) {
         case ASSET_BLANK: return GenColorEco(WHITE); break;
         case ASSET_BUILDMODE_HIGHLIGHT: return GenColorEco(WHITE); break;
 
-        // NOTE(zaklaus): items
-        case ASSET_DEMO_ICEMAKER: return LoadTexEco("demo_icemaker");
-
-        // NOTE(zaklaus): blocks
-        case ASSET_FENCE: return LoadTexEco("fence");
-        case ASSET_GROUND: return LoadTexEco("grass");
-        case ASSET_DIRT: return LoadTexEco("dirt");
-        case ASSET_WALL: return LoadTexEco("asphalt");
-        case ASSET_HILL_SNOW:
-        case ASSET_HILL: return LoadTexEco("rock");
-        case ASSET_LAVA: return LoadTexEco("lava");
-        case ASSET_WOOD: return LoadTexEco("wood");
-        case ASSET_TREE: return LoadTexEco("tree");
-        // case ASSET_WATER: return LoadTexEco("water");
-
-        case ASSET_BELT:
-        case ASSET_BELT_RIGHT: return LoadTexEco("belt_right");
-        case ASSET_BELT_LEFT: return LoadTexEco("belt_left");
-        case ASSET_BELT_UP: return LoadTexEco("belt_up");
-        case ASSET_BELT_DOWN: return LoadTexEco("belt_down");
-
-        // NOTE(zaklaus): devices
-        case ASSET_CHEST: return LoadTexEco("chest");
-
-        default: return GenColorEco(PINK); break;
+        default: return GenColorEco(ColorFromHSV(id * 240.0f, 0.6f, 0.45f)); break;
     }
 }
