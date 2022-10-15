2040984666cb08cd7d0a54623f0cf114b2883eae
Dominik Madarász <zaklaus@outlook.com>
2022-10-15 13:51:22 +0200

    update minimal texgen

--- code/games/minimal/src/texgen.c
+++ code/games/minimal/src/texgen.c
@@ -6,16 +6,12 @@
 Texture2D texgen_build_anim(asset_id id, int64_t counter) {
     (void)counter;
     switch (id) {
-        default: return GenColorEco(ColorFromHSV(id * 240.0f, 0.6f, 0.45f)); break;
+        default: return texgen_build_anim_fallback(id, counter); break;
     }
 }
 
 Texture2D texgen_build_sprite(asset_id id) {
     switch (id) {
-        case ASSET_BLANK: return GenColorEco(WHITE); break;
-        case ASSET_BUILDMODE_HIGHLIGHT: return GenColorEco(WHITE); break;
-        case ASSET_BLOCK_FRAME: return GenFrameRect(); break;
-
-        default: return GenColorEco(ColorFromHSV(id * 240.0f, 0.6f, 0.45f)); break;
+        default: return texgen_build_sprite_fallback(id); break;
     }
 }
